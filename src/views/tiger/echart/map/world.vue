6d8b12702291df143d40440f6a84f9280c14da39
Jason <jason.zhour@gmail.com>
2017-08-08 15:08:50 +0800

    get map data is delay by job

--- src/views/tiger/echart/map/world.vue
+++ src/views/tiger/echart/map/world.vue
@@ -1,23 +1,138 @@
 <template>
-  <div>
-    <todo></todo>
+  <div :class="className" :id="id" :style="{height:height,width:width}">
   </div>
 </template>
 
 <script>
 import echarts from 'echarts';
-import '../../../../assets/echarts/map/world'
+import '../../../../assets/echarts/map/china'
 
 
 export default {
+  props: {
+    className: {
+      type: String,
+      default: 'chart'
+    },
+    id: {
+      type: String,
+      default: 'chart'
+    },
+    width: {
+      type: String,
+      default: '200px'
+    },
+    height: {
+      type: String,
+      default: '200px'
+    }
+  },
   data() {
     return {
-      chart: null
+      chart: null,
+      weiboData
     }
   },
   mounted() {
     this.initChart();
+  },
+  beforeDestroy() {
+    if (!this.chart) {
+      return
+    }
+    this.chart.dispose();
+    this.chat = null;
+  },
+
+  methods: {
+    getWeiboData() {
+      // get data;
+      // http://echarts.baidu.com/gallery/data/asset/data/weibo.json
+    },
+    initChart() {
+      this.chart = echarts.init(document.getElementById(this.id));
+      this.chart.setOption(option = {
+        backgroundColor: '#404a59',
+        title: {
+          text: '微博签到数据点亮中国',
+          subtext: 'From Jason',
+          sublink: '',
+          left: 'center',
+          top: 'top',
+          textStyle: {
+            color: '#fff'
+          }
+        },
+        tooltip: {},
+        legend: {
+          left: 'left',
+          data: ['强', '中', '弱'],
+          textStyle: {
+            color: '#ccc'
+          }
+        },
+        geo: {
+          map: 'china',
+          label: {
+            emphasis: {
+              show: false
+            }
+          },
+          itemStyle: {
+            normal: {
+              areaColor: '#323c48',
+              borderColor: '#111'
+            },
+            emphasis: {
+              areaColor: '#2a333d'
+            }
+          }
+        },
+        series: [{
+          name: '弱',
+          type: 'scatter',
+          coordinateSystem: 'geo',
+          symbolSize: 1,
+          large: true,
+          itemStyle: {
+            normal: {
+              shadowBlur: 2,
+              shadowColor: 'rgba(37, 140, 249, 0.8)',
+              color: 'rgba(37, 140, 249, 0.8)'
+            }
+          },
+          data: weiboData[0]
+        }, {
+          name: '中',
+          type: 'scatter',
+          coordinateSystem: 'geo',
+          symbolSize: 1,
+          large: true,
+          itemStyle: {
+            normal: {
+              shadowBlur: 2,
+              shadowColor: 'rgba(14, 241, 242, 0.8)',
+              color: 'rgba(14, 241, 242, 0.8)'
+            }
+          },
+          data: weiboData[1]
+        }, {
+          name: '强',
+          type: 'scatter',
+          coordinateSystem: 'geo',
+          symbolSize: 1,
+          large: true,
+          itemStyle: {
+            normal: {
+              shadowBlur: 2,
+              shadowColor: 'rgba(255, 255, 255, 0.8)',
+              color: 'rgba(255, 255, 255, 0.8)'
+            }
+          },
+          data: weiboData[2]
+        }]
+      });
+    }
   }
 }
-
 </script>
