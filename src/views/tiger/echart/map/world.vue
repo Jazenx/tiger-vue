2a1338972d08601189d1a48e9019eb2cf24cfb7b
Jason <jason.zhour@gmail.com>
2017-08-08 12:29:24 +0800

    map init

---
+++ src/views/tiger/echart/map/world.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <todo></todo>
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts';
+import '../../../../assets/echarts/map/world'
+
+
+export default {
+  data() {
+    return {
+      chart: null
+    }
+  },
+  mounted() {
+    this.initChart();
+  }
+}
+
+</script>
